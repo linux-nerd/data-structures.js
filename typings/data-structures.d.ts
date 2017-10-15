570c4e1ce83c710e517fbd0212fbe1e19f24c4f3
abhishek <abhishek.prakash@nagarro.com>
2017-10-15 09:35:26 +0530

    Updated typings for BST

--- typings/data-structures.d.ts
+++ typings/data-structures.d.ts
@@ -4,9 +4,16 @@ declare namespace DataStructures {
     lookup(val: string | number): { hasValue: boolean, currentNode: BST, parentNode: BST };
     traverse(type: string): Array<number | string> | Error;
     delete(val: string | number): Error | void;
+    len(): number;
   }
 
   class BSTNode {
     constructor(key: string | number, left?: BST, right?: BST);
+    key(): number | string;
+    key(val: number | string): void;
+    left(): BST;
+    left(node: BST): void;
+    right(): BST;
+    right(node: BST): void;
   }
 }
