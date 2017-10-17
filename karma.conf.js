eb69b2aeea417092b81b40c13f1fcb77ec24d625
Abhishek Prakash <abhishekabby@gmail.com>
2017-10-17 11:01:15 +0530

    Fixed the error

--- karma.conf.js
+++ karma.conf.js
@@ -2,76 +2,77 @@
 // Generated on Mon Oct 02 2017 21:15:08 GMT+0530 (India Standard Time)
 var webpackConfig = require('./webpack.config.js');
 
-var configuration = {
 
-  // base path that will be used to resolve all patterns (eg. files, exclude)
-  basePath: '',
-
-
-  // frameworks to use
-  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-  frameworks: ['jasmine'],
-
-
-  // list of files / patterns to load in the browser
-  files: [
-    'node_modules/babel-polyfill/dist/polyfill.js',
-    'src/**/*.spec.js'
-  ],
-
-  webpack: webpackConfig,
-
-
-  // list of files to exclude
-  exclude: [
-  ],
-
-
-  // preprocess matching files before serving them to the browser
-  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-  preprocessors: {
-    'src/**/*.spec.js': ['webpack'],
-    'src/**/*.js': ['webpack']
-  },
-
-
-  // test results reporter to use
-  // possible values: 'dots', 'progress'
-  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-  reporters: ['progress'],
-
-
-  // web server port
-  port: 9876,
-
-
-  // enable / disable colors in the output (reporters and logs)
-  colors: true,
-
-
-  // level of logging
-  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-  logLevel: config.LOG_INFO,
-
-
-  // enable / disable watching file and executing tests whenever any file changes
-  autoWatch: true,
-
-
-  // start these browsers
-  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-  browsers: ['Chrome', 'PhantomJS'],
-
-
-  // Continuous Integration mode
-  // if true, Karma captures browsers, runs the tests and exits
-  singleRun: false
-};
 
 module.exports = function (config) {
   //workaround for travis
   if (process.env.TRAVIS) {
     configuration.browsers = ['PhantomJS'];
   }
+  var configuration = {
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'node_modules/babel-polyfill/dist/polyfill.js',
+      'src/**/*.spec.js'
+    ],
+
+    webpack: webpackConfig,
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+      'src/**/*.spec.js': ['webpack'],
+      'src/**/*.js': ['webpack']
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome', 'PhantomJS'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: false
+  };
   config.set(configuration);
 }
