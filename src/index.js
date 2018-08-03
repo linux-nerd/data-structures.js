c3ca348a9af9a617f041fe906fef85a47aceb562
Abhishek Prakash <abhishekabby@gmail.com>
2018-08-03 15:01:08 +0530

    Added linked list to the build

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ export * from './queue/queue';
 export * from './binary-search-tree/binary-search-tree';
 export * from './graph/graph';
 export * from './graph/adj-list';
+export * from './linked-list/linked-list';
