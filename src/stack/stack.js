4b69e8832b4037c6d3eef2f57a9dcbc4b3a4e6cc
Abhishek Prakash <abhishekabby@gmail.com>
2018-08-20 15:30:40 +0530

    Updated documentation

--- src/stack/stack.js
+++ src/stack/stack.js
@@ -9,31 +9,51 @@ export class Stack {
     this[_stack] = new LinkedList;
   }
 
+  /**
+   * getter for _stack object
+   */
   get stack() {
     return this[_stack];
   }
 
+  /**
+   * Push an item on top of the stack.
+   * Ideally the insertFirst method of linked list is called to add the item in the first position in linked list
+   * @param {any} item 
+   */
   push(item) {
     this[_stack].insertFirst(item);
   }
 
+  /**
+   * Pop the top item from the stack
+   * Ideally removeFirst method of stack is called
+   */
   pop() {
     if (!this.isEmpty()) {
       return this[_stack].removeFirst().key;
     }
-
   }
 
+  /**
+   * Returns the top item of the stack without popping it
+   */
   peek() {
     if (!this.isEmpty()) {
       return this[_stack].head.key;
     }
   }
 
+  /**
+   * Returns the size of the stack
+   */
   size() {
     return this[_stack].size();
   }
 
+  /**
+   * Checks if the stack is empty
+   */
   isEmpty() {
     return this.size() === 0;
   }
